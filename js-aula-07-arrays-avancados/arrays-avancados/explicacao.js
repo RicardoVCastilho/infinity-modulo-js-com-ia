72095f0e43f94777d50177a7e3c1e83389b26f83
RicardoVCastilho <seuemail@exemplo.com>
2025-07-15 03:32:50 -0300

    Updating class 07

--- js-aula-07-arrays-avancados/arrays-avancados/explicacao.js
+++ js-aula-07-arrays-avancados/arrays-avancados/explicacao.js
@@ -54,7 +54,7 @@ const primeiroPar = numeros3.find(function(numero) {
 
 console.log(`Primeiro número par: ${primeiroPar}`);
 
-// Método find() - Verifica se todos os elementos de um array satisfazem uma determinada condição. Ele retorna true se todos passarem no teste e false se pelo menos um elemento não satisfizer a condição.
+// Método every() - Verifica se todos os elementos de um array satisfazem uma determinada condição. Ele retorna true se todos passarem no teste e false se pelo menos um elemento não satisfizer a condição.
 const numeros4 = [2, 4, 6, 8];
 
 const todosPares = numeros4.every(function(numero) {
